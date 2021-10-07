e5c8c477062fe36f143f0b7547c079a608a47f68
emmanuel <emmanuel.alvarez.a10@gmail.com>
2021-10-06 20:58:32 -0500

    Pruebas de regresión

--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -1,13 +1,13 @@
 const functions = require('./index');
 const context =require('../testing/Context');
 
-test('Http trigger', async ()=>{
+test('Http trigger example', async ()=>{
     const request ={
         query: { name: 'Anica'}
     };
 
     await functions(context, request);
-    debugger;
+    expect(context.res.body).toContain('H');
     expect(context.res.body).toEqual('Hola, Anica');
-    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.log.mock.calls.length).toBe(2);
 });
