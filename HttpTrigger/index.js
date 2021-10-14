4a031ca1c364f5b27bf24fbc9000610f7c3c9746
emmanuel <emmanuel.alvarez.a10@gmail.com>
2021-10-13 23:40:12 -0500

    Pruebas de rendimiento

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -2,12 +2,13 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    
-    if (name){
-     context.res={
-         status: 200,
-         body: "Hola, " + name
-     }; 
+    context.log('JavaScript HTTP trigger function 2.');
+
+    if (name) {
+        context.res = {
+            status: 200,
+            body: "Hola, " + name
+        };
     }
     else {
         context.res = {
@@ -15,11 +16,4 @@ module.exports = async function (context, req) {
             body: "Pass a name in the query string or in the request body"
         };
     }
-    /*const responseMessage = name
-        ? "Hola, " + name 
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 
-        body: responseMessage*/
 }
--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -1,13 +1,18 @@
-const functions = require('./index');
+const httpFunction = require('./index');
 const context =require('../testing/Context');
 
-test('Http trigger example', async ()=>{
-    const request ={
-        query: { name: 'Anica'}
+test('Http trigger example', async () => {
+    const request = {
+        query: { name: 'Anica' }
     };
-
-    await functions(context, request);
+    var iteration = 1000000;
+    console.time('FUNCTION #1');
+    for (var i = 0; i < iteration; i++) {
+        httpFunction(context, request);
+    }
+    console.timeEnd('FUNCTION #1');
+    await httpFunction(context, request);
     expect(context.res.body).toContain('H');
     expect(context.res.body).toEqual('Hola, Anica');
-    expect(context.log.mock.calls.length).toBe(2);
+    //expect(context.log.mock.calls.length).toBe(2);
 });
